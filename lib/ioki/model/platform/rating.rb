edf8250d00d33f697fb2ccc0fbb32a5bb0313ffc
Serge Hänni <serge@nyi.ch>
2022-02-02 12:13:17 +0100

    Change serializers to allow `Rating` to be created

--- lib/ioki/model/platform/rating.rb
+++ lib/ioki/model/platform/rating.rb
@@ -5,14 +5,15 @@ module Ioki
     module Platform
       class Rating < Base
         attribute :version, on: :read, type: :integer
-        attribute :vehicle_rating, on: :read, type: :integer
-        attribute :service_rating, on: :read, type: :integer
-        attribute :driver_rating, on: :read, type: :integer
-        attribute :ride_rating, on: :read, type: :integer
-        attribute :waiting_time_rating, on: :read, type: :integer
-        attribute :punctuality_rating, on: :read, type: :integer
-        attribute :comment, on: :read, type: :string
+        attribute :vehicle_rating, on: [:read, :create], type: :integer
+        attribute :service_rating, on: [:read, :create], type: :integer
+        attribute :driver_rating, on: [:read, :create], type: :integer
+        attribute :ride_rating, on: [:read, :create], type: :integer
+        attribute :waiting_time_rating, on: [:read, :create], type: :integer
+        attribute :punctuality_rating, on: [:read, :create], type: :integer
+        attribute :comment, on: [:read, :create], type: :string
         attribute :editable, on: :read, type: :boolean
+        attribute :ride_version, on: :create, type: :integer
       end
     end
   end
