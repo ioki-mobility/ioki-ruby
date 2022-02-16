61e5c0f70a4cfbc8b1458132dcb0dab9c0833400
Eike Send <eike.send@gmail.com>
2022-02-16 14:04:32 +0100

    Make rubocop happy

--- spec/ioki/model/platform/task_spec.rb
+++ spec/ioki/model/platform/task_spec.rb
@@ -3,24 +3,28 @@
 RSpec.describe Ioki::Model::Platform::Task do
   describe 'new' do
     subject(:task_list) { described_class.new id: '123' }
+
     it { is_expected.to have_attributes(id: '123') }
     it { is_expected.to be_a(described_class) }
   end
 
   describe 'calculated_point' do
     subject(:task_list) { described_class.new calculated_point: { id: '123' } }
+
     it { is_expected.to have_attributes calculated_point: have_attributes(id: '123') }
     it { is_expected.to have_attributes calculated_point: be_a(Ioki::Model::Platform::CalculatedPoint) }
   end
 
   describe 'pause' do
     subject(:task_list) { described_class.new pause: { id: '123' } }
+
     it { is_expected.to have_attributes pause: have_attributes(id: '123') }
     it { is_expected.to have_attributes pause: be_a(Ioki::Model::Platform::Pause) }
   end
 
   describe 'ride' do
     subject(:task_list) { described_class.new ride: { id: '123' } }
+
     it { is_expected.to have_attributes ride: have_attributes(id: '123') }
     it { is_expected.to have_attributes ride: be_a(Ioki::Model::Platform::Ride) }
   end
