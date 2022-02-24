22c50aafa7bfe3ce3f3e048111cc33c7c047fea4
Serge Hänni <serge@nyi.ch>
2022-02-24 20:05:25 +0100

    Add phone verification requests for the platform API

---
+++ spec/ioki/model/platform/phone_verification_request_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Ioki::Model::Platform::PhoneVerificationRequest do
+  it { is_expected.to define_attribute(:id).as(:string) }
+  it { is_expected.to define_attribute(:type).as(:string) }
+  it { is_expected.to define_attribute(:created_at).as(:date_time) }
+  it { is_expected.to define_attribute(:updated_at).as(:date_time) }
+
+  it { is_expected.to define_attribute(:phone_number).as(:string) }
+  it { is_expected.to define_attribute(:locale).as(:string) }
+end
