5d33fecbd72f74e9e24429fae3853676f5cdafc0
Tobias Matz <tobias.matz@ioki.com>
2022-03-16 15:53:09 +0100

    Refine Model::Operator::Vehicle

--- lib/ioki/model/operator/vehicle.rb
+++ lib/ioki/model/operator/vehicle.rb
@@ -4,22 +4,22 @@ module Ioki
   module Model
     module Operator
       class Vehicle < Base
-        attribute :connected_driver_id, on: :read, type: :string
-        attribute :fuel_type,           on: :read, type: :string
-        attribute :license_plate,       on: :read, type: :string
-        attribute :manufacturer,        on: :read, type: :string
-        attribute :model,               on: :read, type: :string
-        attribute :nickname,            on: :read, type: :string
-        attribute :seats,               on: :read, type: :integer
-        attribute :storage_spaces,      on: :read, type: :integer
-        attribute :walker_bays,         on: :read, type: :integer
-        attribute :wheelchair_bays,     on: :read, type: :integer
-        attribute :autonomous, on: :read, type: :boolean
-        attribute :description, on: :read, type: :string
-        attribute :external_id, on: :read, type: :string
-        attribute :last_known_position, on: :read, type: :object, class_name: 'VehiclePosition'
-        attribute :phone_number, on: :read, type: :string
-        attribute :version, on: :read, type: :integer
+        attribute :connected_driver_id, on: [:read], type: :string
+        attribute :fuel_type,           on: [:read], type: :string
+        attribute :license_plate,       on: [:read, :create], type: :string
+        attribute :manufacturer,        on: [:read], type: :string
+        attribute :model,               on: [:read], type: :string
+        attribute :nickname,            on: [:read, :create], type: :string
+        attribute :seats,               on: [:read], type: :integer
+        attribute :storage_spaces,      on: [:read], type: :integer
+        attribute :walker_bays,         on: [:read], type: :integer
+        attribute :wheelchair_bays,     on: [:read], type: :integer
+        attribute :autonomous, on: [:read], type: :boolean
+        attribute :description, on: [:read], type: :string
+        attribute :external_id, on: [:read], type: :string
+        attribute :last_known_position, on: [:read], type: :object, class_name: 'VehiclePosition'
+        attribute :phone_number, on: [:read, :create], type: :string
+        attribute :version, on: [:read], type: :integer
       end
     end
   end
