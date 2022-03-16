26601a2d7b1ef9b790cb51e855d8b4562e218dba
Tobias Matz <tobias.matz@ioki.com>
2022-03-16 16:10:31 +0100

    Adjust model attributes

--- lib/ioki/model/operator/vehicle.rb
+++ lib/ioki/model/operator/vehicle.rb
@@ -4,22 +4,80 @@ module Ioki
   module Model
     module Operator
       class Vehicle < Base
-        attribute :connected_driver_id, on: [:read], type: :string
-        attribute :fuel_type,           on: [:read], type: :string
-        attribute :license_plate,       on: [:read, :create], type: :string
-        attribute :manufacturer,        on: [:read], type: :string
-        attribute :model,               on: [:read], type: :string
-        attribute :nickname,            on: [:read, :create], type: :string
-        attribute :seats,               on: [:read], type: :integer
-        attribute :storage_spaces,      on: [:read], type: :integer
-        attribute :walker_bays,         on: [:read], type: :integer
-        attribute :wheelchair_bays,     on: [:read], type: :integer
-        attribute :autonomous, on: [:read], type: :boolean
-        attribute :description, on: [:read], type: :string
-        attribute :external_id, on: [:read], type: :string
-        attribute :last_known_position, on: [:read], type: :object, class_name: 'VehiclePosition'
-        attribute :phone_number, on: [:read, :create], type: :string
-        attribute :version, on: [:read], type: :integer
+        attribute :connected_driver_id,
+                  on:   :read,
+                  type: :string
+
+        attribute :fuel_type,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :license_plate,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :manufacturer,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :model,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :nickname,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :seats,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :storage_spaces,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :walker_bays,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :wheelchair_bays,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :autonomous,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :boolean
+
+        attribute :description, 
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :external_id,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :last_known_position,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'VehiclePosition'
+
+        attribute :phone_number,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :version,
+                  on:   :read,
+                  type: :integer
       end
     end
   end
