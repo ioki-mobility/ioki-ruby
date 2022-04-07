babc3ea29eb3697ff5ddf5443213687be8473cd2
Eike Send <eike.send@gmail.com>
2022-04-07 14:46:42 +0200

    Fixes Driver API vehicle_position test (and rubocop)

--- spec/ioki/driver_api_spec.rb
+++ spec/ioki/driver_api_spec.rb
@@ -128,7 +128,7 @@ RSpec.describe Ioki::DriverApi do
       expect(driver_client).to receive(:request) do |params|
         expect(params[:url].to_s).to eq('driver/vehicle/positions')
         expect(params[:method]).to eq(:post)
-        expect(params[:body]).to include(data: hash_including(lat: 1.0, lng: 2.0, recorded_at: '2012-12-12T12:12:12Z'))
+        expect(params[:body]).to include(data: hash_including(lat: 1.0, lng: 2.0, recorded_at: be_a(DateTime)))
         [nil, full_response]
       end
 
@@ -205,22 +205,24 @@ RSpec.describe Ioki::DriverApi do
   describe 'rides' do
     let(:ride) { Ioki::Model::Driver::Ride.new id: 'xyz_123' }
     let(:passengers) { Ioki::Model::Driver::Passengers.new passengers: [passenger], ride_version: 0 }
-    let(:passenger) { Ioki::Model::Driver::Passenger.new type: "adult", first_name: "Max", last_name: "Mustermann" }
+    let(:passenger) { Ioki::Model::Driver::Passenger.new type: 'adult', first_name: 'Max', last_name: 'Mustermann' }
+
     it '#ride_fare_preview calls request on the client with expected params' do
       expect(driver_client).to receive(:request) do |params|
         expect(params[:url].to_s).to eq('driver/rides/xyz_123/fare_preview')
         expect(params[:method]).to eq(:post)
-        expect(params[:body]).to include(data: include(passengers: all(include(type: "adult"))))
+        expect(params[:body]).to include(data: include(passengers: all(include(type: 'adult'))))
 
         [{ 'data' => { id: 'preview' } }, full_response]
       end
       expect(driver_client.ride_fare_preview(ride, passengers)).to be_a(Ioki::Model::Driver::Fare)
     end
+
     it '#ride_passengers calls request on the client with expected params' do
       expect(driver_client).to receive(:request) do |params|
         expect(params[:url].to_s).to eq('driver/rides/xyz_123/passengers')
         expect(params[:method]).to eq(:post)
-        expect(params[:body]).to include(data: include(ride_version: 0, passengers: all(include(type: "adult"))))
+        expect(params[:body]).to include(data: include(ride_version: 0, passengers: all(include(type: 'adult'))))
 
         [{ 'data' => { id: 'rid_123' } }, full_response]
       end
