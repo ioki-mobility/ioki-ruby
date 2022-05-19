6d8c45dae3fab512c8c540e5b3f7caf1d2653a78
Ozan Bahar <ozan.bahar@ioki.com>
2022-05-19 12:44:24 +0200

    Add line class and spec

---
+++ lib/ioki/model/platform/line.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Platform
+      class Line < Base
+        attribute :type, on: :read, type: :string
+        attribute :id, on: :read, type: :string
+        attribute :created_at, on: :read, type: :date_time
+        attribute :updated_at, on: :read, type: :date_time
+        attribute :name, on: [:read, :create], type: :string
+        attribute :route_number, on: :read, type: :string
+        attribute :variant, on: :read, type: :string
+        attribute :skip_time_window_check, on: :read, type: :boolean
+        attribute :line_stops, type: :array, on: :read, class_name: 'LineStop'
+        attribute :version, on: :read, type: :integer
+      end
+    end
+  end
+end
