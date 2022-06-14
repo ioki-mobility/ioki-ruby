a4d8e4ed2cf1e0f61a00730a866e4a7ff74f9bea
Tobias Matz <tobias.matz@ioki.com>
2022-06-14 08:22:22 +0200

    Put attributes of Model::Operator::Vehicle in alphabetical order

--- lib/ioki/model/operator/vehicle.rb
+++ lib/ioki/model/operator/vehicle.rb
@@ -4,18 +4,50 @@ module Ioki
   module Model
     module Operator
       class Vehicle < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :autonomous,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :boolean
+
         attribute :connected_driver_id,
                   on:   :read,
                   type: :string
+
+        attribute :description,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :external_id,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
 
         attribute :fuel_type,
                   on:             [:create, :read, :update],
                   omit_if_nil_on: [:create, :update],
                   type:           :string
+
+        attribute :last_known_position,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'VehiclePosition'
 
         attribute :license_plate,
                   on:   [:create, :read, :update],
@@ -32,6 +64,10 @@ module Ioki
                   type:           :string
 
         attribute :nickname,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :phone_number,
                   on:   [:create, :read, :update],
                   type: :string
 
@@ -54,30 +90,6 @@ module Ioki
                   on:             [:create, :read, :update],
                   omit_if_nil_on: [:create, :update],
                   type:           :integer
-
-        attribute :autonomous,
-                  on:             [:create, :read, :update],
-                  omit_if_nil_on: [:create, :update],
-                  type:           :boolean
-
-        attribute :description,
-                  on:             [:create, :read, :update],
-                  omit_if_nil_on: [:create, :update],
-                  type:           :string
-
-        attribute :external_id,
-                  on:             [:create, :read, :update],
-                  omit_if_nil_on: [:create, :update],
-                  type:           :string
-
-        attribute :last_known_position,
-                  on:         :read,
-                  type:       :object,
-                  class_name: 'VehiclePosition'
-
-        attribute :phone_number,
-                  on:   [:create, :read, :update],
-                  type: :string
 
         attribute :version,
                   on:   :read,
