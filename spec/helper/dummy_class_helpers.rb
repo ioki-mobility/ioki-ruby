2c80e1c93afb9c348885c76f4175da92989f936d
Tobias Matz <tobias.matz@ioki.com>
2022-07-15 13:20:14 +0200

    Add dummy_class_helpers

---
+++ spec/helper/dummy_class_helpers.rb
@@ -0,0 +1,16 @@
+module Dummys
+end
+
+module DummyClassHelpers
+  def dummy_class(name, module_name, superclass = Object, &block)
+    let(name.to_s) do
+      klass = Class.new(superclass, &block)
+
+      module_name.const_set (name.to_s + SecureRandom.hex(5)).split('_').collect!{ |w| w.capitalize }.join, klass
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.extend DummyClassHelpers
+end
