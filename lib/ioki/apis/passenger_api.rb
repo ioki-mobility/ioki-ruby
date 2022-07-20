1332105dfd049072413a3f97c772d2cac73888b5
Serge Hänni <serge@nyi.ch>
2022-07-20 18:39:48 +0200

    Add endpoint for available notification settings
    [#1096]

--- lib/ioki/apis/passenger_api.rb
+++ lib/ioki/apis/passenger_api.rb
@@ -96,6 +96,12 @@ module Ioki
         base_path:   [API_BASE_PATH],
         model_class: Ioki::Model::Passenger::NotificationSettings
       ),
+      Endpoints::ShowSingular.new(
+        :available_notification_settings,
+        path:        ['passenger', 'notification_settings/available'],
+        base_path:   [API_BASE_PATH],
+        model_class: Ioki::Model::Passenger::NotificationSettings
+      ),
       Endpoints::UpdateSingular.new(
         :notification_settings,
         path:        'notification_settings',
