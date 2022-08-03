c29490a5f1d6faefe9796f0649410ee3b05b8065
Tobias Matz <tobias.matz@ioki.com>
2022-08-03 13:05:38 +0200

    Refine MatchingConfiguration model

--- lib/ioki/model/operator/matching_configuration.rb
+++ lib/ioki/model/operator/matching_configuration.rb
@@ -4,13 +4,34 @@ module Ioki
   module Model
     module Operator
       class MatchingConfiguration < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :area, on: :read, type: :object, class_name: 'Area'
-        attribute :name, on: :read, type: :string
-        attribute :slug, on: :read, type: :string
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :area,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'Area'
+
+        attribute :name,
+                  on:   :read,
+                  type: :string
+
+        attribute :slug,
+                  on:   :read,
+                  type: :string
       end
     end
   end
