c4861da775f7988b14b6453f7786a04c5e5da55a
Tobias Matz <tobias.matz@ioki.com>
2022-09-23 10:22:26 +0200

    Refine Model::Operator::RidePassenger

--- lib/ioki/model/operator/ride_passenger.rb
+++ lib/ioki/model/operator/ride_passenger.rb
@@ -4,12 +4,29 @@ module Ioki
   module Model
     module Operator
       class RidePassenger < Base
-        attribute :type,                    type: :string, on: [:create, :update]
-        attribute :bahncard,                type: :boolean, on: [:create, :update], omit_if_blank_on: [:create, :update]
-        attribute :blue_badge,              type: :boolean, on: [:create, :update], omit_if_blank_on: [:create, :update]
-        attribute :public_transport_ticket, type: :boolean, on: [:create, :update], omit_if_blank_on: [:create, :update]
-        attribute :walker,                  type: :boolean, on: [:create, :update], omit_if_blank_on: [:create, :update]
-        attribute :wheelchair,              type: :boolean, on: [:create, :update], omit_if_blank_on: [:create, :update]
+        attribute :bahncard,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :blue_badge,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :public_transport_ticket,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :walker,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :wheelchair,
+                  on:   :read,
+                  type: :boolean
       end
     end
   end
