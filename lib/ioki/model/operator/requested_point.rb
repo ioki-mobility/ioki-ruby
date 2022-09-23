24b3c09f9148d1943c709e606ca6d871ac454fe9
Tobias Matz <tobias.matz@ioki.com>
2022-09-23 10:21:59 +0200

    Refine Model::Operator::RequestedPoint

--- lib/ioki/model/operator/requested_point.rb
+++ lib/ioki/model/operator/requested_point.rb
@@ -4,24 +4,77 @@ module Ioki
   module Model
     module Operator
       class RequestedPoint < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :city,             type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :country,          type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :county,           type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :display_times,    type: :array,     on: [:create, :read], omit_if_blank_on: :create
-        attribute :formatted_street, type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :lat,              type: :float,     on: [:create, :read], omit_if_blank_on: :create
-        attribute :lng,              type: :float,     on: [:create, :read], omit_if_blank_on: :create
-        attribute :location_name,    type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :postal_code,      type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :station_id,       type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :street_name,      type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :street_number,    type: :string,    on: [:create, :read], omit_if_blank_on: :create
-        attribute :time,             type: :date_time, on: [:create, :read], omit_if_blank_on: :create
-        attribute :waypoint_type,    type: :string,    on: [:create, :read], omit_if_blank_on: :create
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :city,
+                  on:   :read,
+                  type: :string
+
+        attribute :country,
+                  on:   :read,
+                  type: :string
+
+        attribute :county,
+                  on:   :read,
+                  type: :string
+
+        attribute :display_times,
+                  on:   :read,
+                  type: :array
+
+        attribute :formatted_street,
+                  on:   :read,
+                  type: :string
+
+        attribute :lat,
+                  on:   :read,
+                  type: :float
+
+        attribute :lng,
+                  on:   :read,
+                  type: :float
+
+        attribute :location_name,
+                  on:   :read,
+                  type: :string
+
+        attribute :postal_code,
+                  on:   :read,
+                  type: :string
+
+        attribute :station_id,
+                  on:   :read,
+                  type: :string
+
+        attribute :street_name,
+                  on:   :read,
+                  type: :string
+
+        attribute :street_number,
+                  on:   :read,
+                  type: :string
+
+        attribute :time,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :waypoint_type,
+                  on:   :read,
+                  type: :string
       end
     end
   end
