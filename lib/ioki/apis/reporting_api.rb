995502c292f281d70fdaf0dd224e45572758eab4
Tobias Matz <tobias.matz@ioki.com>
2022-09-28 12:10:51 +0200

    Adjust API_BASE_PATH for ReportingApi

--- lib/ioki/apis/reporting_api.rb
+++ lib/ioki/apis/reporting_api.rb
@@ -5,9 +5,16 @@ require 'ioki/apis/endpoints/endpoints'
 
 module Ioki
   class ReportingApi
-    API_BASE_PATH = 'reporting'
+    # for now the api base path is 'operator' this will change
+    # as soon as reporting has its own namespace in triebwerk
+    API_BASE_PATH = 'operator'
     ENDPOINTS = [
-
+      Endpoints.crud_endpoints(
+        :incidence,
+        base_path:   [API_BASE_PATH, 'reporting'],
+        model_class: Ioki::Model::Reporting::Incidence,
+        except:      [:create, :update, :delete, :show]
+      )
     ].freeze
   end
 end
