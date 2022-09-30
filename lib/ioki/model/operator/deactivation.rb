b3165084160701c5772792c6821fa88f777fb5d0
Tobias Matz <tobias.matz@ioki.com>
2022-09-30 11:23:58 +0200

    Setup deactivation model

--- lib/ioki/model/operator/deactivation.rb
+++ lib/ioki/model/operator/deactivation.rb
@@ -4,12 +4,29 @@ module Ioki
   module Model
     module Operator
       class Deactivation < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :ends_at, on: :read, type: :date_time
-        attribute :starts_at, on: :read, type: :date_time
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :ends_at,
+                  on:   [:create, :read, :update],
+                  type: :date_time
+
+        attribute :starts_at,
+                  on:   [:create, :read, :update]
+                  type: :date_time
       end
     end
   end
