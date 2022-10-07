b0064a907ba01713bfd3db8517752f3edcd4ad59
Tobias Matz <tobias.matz@ioki.com>
2022-10-07 11:11:35 +0200

    Add station_category_id to Station model

--- lib/ioki/model/operator/station.rb
+++ lib/ioki/model/operator/station.rb
@@ -81,6 +81,10 @@ module Ioki
                   on:   [:read, :create, :update],
                   type: :string
 
+        attribute :station_category_id,
+                  on:   [:create, :update, :read],
+                  type: :string
+
         attribute :station_type,
                   on:             [:read, :create, :update],
                   omit_if_nil_on: [:create, :update],
