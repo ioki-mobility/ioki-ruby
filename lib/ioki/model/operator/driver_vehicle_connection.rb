f02624f01778aa0c62e9ace6e5dbaa5fc28d626e
Tobias Matz <tobias.matz@ioki.com>
2022-10-11 12:05:34 +0200

    Refine DriverVehicleConnection model

--- lib/ioki/model/operator/driver_vehicle_connection.rb
+++ lib/ioki/model/operator/driver_vehicle_connection.rb
@@ -4,12 +4,39 @@ module Ioki
   module Model
     module Operator
       class DriverVehicleConnection < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: [:update, :read], type: :string
-        attribute :vehicle, on: :read, type: :object, class_name: 'Vehicle'
-        attribute :driver, on: :read, type: :object, class_name: 'Driver'
-        attribute :activated_at, on: :read, type: :date_time
-        attribute :deactivated_at, on: [:create, :read], type: :date_time, omit_if_blank_on: :create
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :activated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :deactivated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :driver,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'Driver'
+
+        attribute :driver_id,
+                  on:   [:create, :read],
+                  type: :string
+
+        attribute :vehicle,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'Vehicle'
+
+        attribute :vehicle_id,
+                  on:   [:create, :read],
+                  type: :string
       end
     end
   end
