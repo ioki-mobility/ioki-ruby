52c510e27b4d9d7020088deb157ca293137413b4
Tobias Matz <tobias.matz@ioki.com>
2022-10-17 14:42:28 +0200

    Refine Rating model

--- lib/ioki/model/operator/rating.rb
+++ lib/ioki/model/operator/rating.rb
@@ -4,19 +4,61 @@ module Ioki
   module Model
     module Operator
       class Rating < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :version, on: :read, type: :integer
-        attribute :vehicle_rating, on: :read, type: :integer
-        attribute :service_rating, on: :read, type: :integer
-        attribute :driver_rating, on: :read, type: :integer
-        attribute :ride_rating, on: :read, type: :integer
-        attribute :waiting_time_rating, on: :read, type: :integer
-        attribute :punctuality_rating, on: :read, type: :integer
-        attribute :comment, on: :read, type: :string
-        attribute :editable, on: :read, type: :boolean
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :comment,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :driver_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :editable,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :punctuality_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :ride_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :service_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :vehicle_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :vehicle_cleanliness_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :version,
+                  on:   [:create, :read, :update],
+                  type: :integer
+
+        attribute :waiting_time_rating,
+                  on:   [:create, :read, :update],
+                  type: :integer
       end
     end
   end
