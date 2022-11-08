b440c07e1e6d90c37c072c49ff91ead42f68c952
Tobias Matz <tobias.matz@ioki.com>
2022-11-08 14:55:46 +0100

    Refine Journey and Leg model

--- lib/ioki/model/operator/journey.rb
+++ lib/ioki/model/operator/journey.rb
@@ -4,12 +4,30 @@ module Ioki
   module Model
     module Operator
       class Journey < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :legs, type: :array, on: :read, class_name: 'Leg'
-        attribute :version, type: :integer, on: :read
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :legs,
+                  on:         :read,
+                  type:       :array,
+                  class_name: 'Leg'
+
+        attribute :version,
+                  on:   :read,
+                  type: :integer
       end
     end
   end
