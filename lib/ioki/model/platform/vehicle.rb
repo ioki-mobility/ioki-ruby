38c1983aa4d40560993c259b1d9d58b9f8cf1b9d
Tobias Matz <tobias.matz@ioki.com>
2022-12-09 11:40:19 +0100

    Fix failing spec

--- lib/ioki/model/platform/vehicle.rb
+++ lib/ioki/model/platform/vehicle.rb
@@ -63,7 +63,7 @@ module Ioki
 
         attribute :operator_id,
                   on:             [:create, :read, :update],
-                  omit_if_nil_on: [:create, :updated],
+                  omit_if_nil_on: [:create, :update],
                   type:           :string
 
         attribute :phone_number,
