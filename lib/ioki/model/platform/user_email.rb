29f45a43b104f690e7e3f7d12ed9264b39ada204
Tobias Matz <tobias.matz@ioki.com>
2023-01-05 22:16:26 +0100

    Add confirmed_email_address and unconfirmed_email_address to UserEmail

--- lib/ioki/model/platform/user_email.rb
+++ lib/ioki/model/platform/user_email.rb
@@ -6,10 +6,29 @@ module Ioki
       class UserEmail < Base
         unvalidated true
 
-        attribute :confirmed,     on: :read, type: :boolean
-        attribute :email_address, on: [:create, :read, :update], type: :string
-        attribute :newsletter,    on: [:create, :read, :update], type: :boolean
-        attribute :receipt,       on: [:create, :read, :update], type: :boolean
+        attribute :confirmed,
+                  on:   :read,
+                  type: :boolean
+
+        attribute :confirmed_email_address,
+                  on:   [:create, :update],
+                  type: :string
+
+        attribute :email_address,
+                  on:   [:create, :read, :update],
+                  type: :string
+
+        attribute :newsletter,
+                  on:   [:create, :read, :update],
+                  type: :boolean
+
+        attribute :receipt,
+                  on:   [:create, :read, :update],
+                  type: :boolean
+
+        attribute :unconfirmed_email_address,
+                  on:   [:create, :update],
+                  type: :string
       end
     end
   end
