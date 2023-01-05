349709314f205efab1eea998d0838f64b16ebaa3
Serge Hänni <serge@nyi.ch>
2023-01-05 22:05:56 +0100

    Add `paypal_secure_element`
    [#1620]

--- lib/ioki/model/passenger/service_credit_create.rb
+++ lib/ioki/model/passenger/service_credit_create.rb
@@ -7,6 +7,7 @@ module Ioki
         attribute :cost, type: :integer, on: :create
         attribute :value, type: :integer, on: :create
         attribute :payment_method, type: :object, on: :create, class_name: 'PaymentMethod'
+        attribute :paypal_secure_element, type: :string, on: :create, omit_if_nil_on: :create
       end
     end
   end
