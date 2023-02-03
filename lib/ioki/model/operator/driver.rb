a98da5572d93122e5410f8a157baa1f87e208c57
Tobias Matz <tobias.matz@ioki.com>
2023-02-03 14:00:45 +0100

    Add external_id to Driver model

--- lib/ioki/model/operator/driver.rb
+++ lib/ioki/model/operator/driver.rb
@@ -28,6 +28,11 @@ module Ioki
                   on:   :read,
                   type: :string
 
+        attribute :external_id,
+                  on:             [:create, :read, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
         attribute :first_name,
                   on:             [:create, :read, :update],
                   omit_if_nil_on: [:create, :update],
