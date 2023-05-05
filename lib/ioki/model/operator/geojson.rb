c42eb8661f3c6561ba99751b8fa09e209c11748d
Ozan Bahar <ozan.bahar@ioki.com>
2023-05-05 12:08:19 +0200

    Rename area to geojson and update references

---
+++ lib/ioki/model/operator/geojson.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class Geojson < Base
+        unvalidated true
+
+        attribute :coordinates, type: :array, on: :read
+        attribute :type, type: :string, on: :read
+      end
+    end
+  end
+end
