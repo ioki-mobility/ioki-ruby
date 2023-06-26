f6dcaf2302cf2f797203aa8129683e20cb0bf7e9
Robin Mehner <robin@coding-robin.de>
2023-06-26 11:48:40 +0200

    Apply new Rubocop rule about String literals in RegExp

--- spec/helper/string_helper.rb
+++ spec/helper/string_helper.rb
@@ -4,7 +4,7 @@ module StringHelper
   def self.underscore(camel_cased_word)
     camel_cased_word
       .to_s
-      .gsub(/::/, '/')
+      .gsub('::', '/')
       .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
       .gsub(/([a-z\d])([A-Z])/, '\1_\2')
       .tr('-', '_')
