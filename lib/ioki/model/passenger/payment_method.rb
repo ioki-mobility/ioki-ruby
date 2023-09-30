0258640a31b2d85d6db6ec895fc4dd402b92676a
Eike Send <mail@eike.se>
2023-09-30 11:09:45 +0200

    Add all missing schema_path / unvalidated to all models

--- lib/ioki/model/passenger/payment_method.rb
+++ lib/ioki/model/passenger/payment_method.rb
@@ -4,6 +4,10 @@ module Ioki
   module Model
     module Passenger
       class PaymentMethod < Base
+        def self.schema_path
+          'passenger_api--payment_method_schema'
+        end
+
         attribute :type, on: :read, type: :string
         attribute :id, on: [:read, :create], omit_if_nil_on: :create, type: :string
         attribute :created_at, on: :read, type: :date_time
