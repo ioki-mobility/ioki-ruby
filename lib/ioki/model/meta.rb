5d7360b8f7e41b9e736691ee3a8ad11e68b78051
Tobias Matz <tobias.matz@ioki.com>
2023-10-12 21:08:43 +0200

    Add more data to Ioki::Model::Meta

--- lib/ioki/model/meta.rb
+++ lib/ioki/model/meta.rb
@@ -3,11 +3,13 @@
 module Ioki
   module Model
     class Meta
-      attr_reader :page, :last_page
+      attr_reader :count, :page, :last_page, :total_pages
 
       def initialize(attributes)
+        @count = attributes['count']
         @page = attributes['page']
         @last_page = attributes['last_page']
+        @total_pages = attributes['total_pages']
       end
     end
   end
