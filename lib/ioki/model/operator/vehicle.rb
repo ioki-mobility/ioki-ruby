57fef9d6c7a05c4672610fad7e4806d0ab02ba57
Serge Hänni <serge@nyi.ch>
2023-10-30 15:41:46 +0100

    Remove the `operator_id` from update
    The operator can only be set when creating a vehicle, but not when updating.

--- lib/ioki/model/operator/vehicle.rb
+++ lib/ioki/model/operator/vehicle.rb
@@ -89,8 +89,8 @@ module Ioki
                   type:           :integer
 
         attribute :operator_id,
-                  on:             [:create, :read, :update],
-                  omit_if_nil_on: [:create, :update],
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
                   type:           :string
 
         attribute :phone_number,
