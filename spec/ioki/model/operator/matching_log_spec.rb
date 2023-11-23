83984fa9b25dad0de72332c410abd0dc24a4ef89
Tobias Matz <64846810+tom-ioki@users.noreply.github.com>
2023-11-23 15:22:18 +0100

    Revert "Operator Api | Add matching logs endpoint"

--- spec/ioki/model/operator/matching_log_spec.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.describe Ioki::Model::Operator::MatchingLog do
-  it { is_expected.to define_attribute(:dt).as(:integer) }
-  it { is_expected.to define_attribute(:log_level).as(:integer) }
-  it { is_expected.to define_attribute(:message).as(:string) }
-end
