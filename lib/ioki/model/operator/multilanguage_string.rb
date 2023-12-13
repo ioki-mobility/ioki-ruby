3e1ffb36d098d2ee6d5a3765e049f58994a8735a
Tobias Matz <tobias.matz@ioki.com>
2023-12-13 19:16:27 +0100

    Add MultilanguageString model

---
+++ lib/ioki/model/operator/multilanguage_string.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class MultilanguageString < Base
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :name,
+                  on:   :read,
+                  type: :string
+
+        attribute :translations,
+                  on:         :read,
+                  type:       :array,
+                  class_name: 'Translation'
+      end
+    end
+  end
+end
