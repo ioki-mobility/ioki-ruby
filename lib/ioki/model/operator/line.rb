35cdb86e920cbc08fd6d3fd493ec9e1854ba7fca
Tobias Matz <tobias.matz@ioki.com>
2024-01-17 19:58:28 +0100

    Fix line model

--- lib/ioki/model/operator/line.rb
+++ lib/ioki/model/operator/line.rb
@@ -4,17 +4,54 @@ module Ioki
   module Model
     module Operator
       class Line < Base
-        attribute :type, on: :read, type: :string
-        attribute :id, on: :read, type: :string
-        attribute :created_at, on: :read, type: :date_time
-        attribute :updated_at, on: :read, type: :date_time
-        attribute :name, on: [:read, :create, :update], type: :string
-        attribute :mode, on: [:read, :create, :update], type: :string
-        attribute :route_number, on: [:read, :create, :update], type: :string
-        attribute :skip_time_window_check, on: [:read, :create, :update], type: :boolean
-        attribute :slug, on: [:read, :create, :update], type: :string
-        attribute :variant, on: [:read, :create, :update], type: :string
-        attribute :line_stops, type: :array, on: :read, class_name: 'LineStop'
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :name,
+                  on:             [:read, :create, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :mode,
+                  on:             [:read, :create, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :string
+
+        attribute :route_number,
+                  on:   [:read, :create, :update],
+                  type: :string
+
+        attribute :skip_time_window_check,
+                  on:             [:read, :create, :update],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :boolean
+
+        attribute :slug,
+                  on:             [:read, :create, :update],
+                  type:           :string,
+                  omit_if_nil_on: [:create, :update]
+
+        attribute :variant,
+                  on:   [:read, :create, :update],
+                  type: :string
+
+        attribute :line_stops,
+                  type:       :array,
+                  on:         :read,
+                  class_name: 'LineStop'
       end
     end
   end
