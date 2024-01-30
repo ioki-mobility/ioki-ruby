ee457ffaa07b8fc7f69596a1cbe88468a2499c1a
Serge Hänni <serge@nyi.ch>
2024-01-30 10:00:55 +0100

    Add `Provider#features.analytics_tracking` to the platform API

--- lib/ioki/model/platform/provider_features.rb
+++ lib/ioki/model/platform/provider_features.rb
@@ -6,6 +6,7 @@ module Ioki
       class ProviderFeatures < Base
         unvalidated true # Specification not available
 
+        attribute :analytics_tracking, type: :boolean, on: :read
         attribute :failed_payment_handling, type: :boolean, on: :read
         attribute :payment, type: :boolean, on: :read
         attribute :promo_codes, type: :boolean, on: :read
