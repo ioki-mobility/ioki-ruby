acba3ef412bf520080389196b9711e83415f78c3
Tobias Matz <tobias.matz@ioki.com>
2024-01-31 18:31:20 +0100

    add missing attributes to ResourceConfiguration

--- lib/ioki/model/operator/resource_configuration.rb
+++ lib/ioki/model/operator/resource_configuration.rb
@@ -20,16 +20,81 @@ module Ioki
                   on:   :read,
                   type: :date_time
 
+        attribute :bicycle_racks,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :child_booster_seat,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :child_safety_seat,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
         attribute :default,
                   on:   :read,
                   type: :boolean
+
+        attribute :hand_luggage_storage_spaces,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :medical_seats,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
 
         attribute :name,
                   on:             [:create, :update, :read],
                   omit_if_nil_on: [:create, :update],
                   type:           :string
 
+        attribute :pet_transportation_bays,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :power_plugs,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :resource_1,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :resource_2,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :resource_3,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :resource_4,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :resource_5,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
         attribute :seats,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :special_seats,
                   on:             [:create, :update, :read],
                   omit_if_nil_on: [:create, :update],
                   type:           :integer
@@ -39,12 +104,32 @@ module Ioki
                   omit_if_nil_on: [:create, :update],
                   type:           :integer
 
+        attribute :suitcase_storage_spaces,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
         attribute :walker_bays,
                   on:             [:create, :update, :read],
                   omit_if_nil_on: [:create, :update],
                   type:           :integer
 
+        attribute :walker_storage_spaces,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
         attribute :wheelchair_bays,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :wheelchair_storage_spaces,
+                  on:             [:create, :update, :read],
+                  omit_if_nil_on: [:create, :update],
+                  type:           :integer
+
+        attribute :wlan_access_points,
                   on:             [:create, :update, :read],
                   omit_if_nil_on: [:create, :update],
                   type:           :integer
--- spec/ioki/model/operator/resource_configuration_spec.rb
+++ spec/ioki/model/operator/resource_configuration_spec.rb
@@ -5,10 +5,27 @@ RSpec.describe Ioki::Model::Operator::ResourceConfiguration do
   it { is_expected.to define_attribute(:type).as(:string) }
   it { is_expected.to define_attribute(:created_at).as(:date_time) }
   it { is_expected.to define_attribute(:updated_at).as(:date_time) }
+  it { is_expected.to define_attribute(:bicycle_racks).as(:integer) }
+  it { is_expected.to define_attribute(:child_booster_seat).as(:integer) }
+  it { is_expected.to define_attribute(:child_safety_seat).as(:integer) }
   it { is_expected.to define_attribute(:default).as(:boolean) }
+  it { is_expected.to define_attribute(:hand_luggage_storage_spaces).as(:integer) }
+  it { is_expected.to define_attribute(:medical_seats).as(:integer) }
   it { is_expected.to define_attribute(:name).as(:string) }
+  it { is_expected.to define_attribute(:pet_transportation_bays).as(:integer) }
+  it { is_expected.to define_attribute(:power_plugs).as(:integer) }
+  it { is_expected.to define_attribute(:resource_1).as(:integer) }
+  it { is_expected.to define_attribute(:resource_2).as(:integer) }
+  it { is_expected.to define_attribute(:resource_3).as(:integer) }
+  it { is_expected.to define_attribute(:resource_4).as(:integer) }
+  it { is_expected.to define_attribute(:resource_5).as(:integer) }
   it { is_expected.to define_attribute(:seats).as(:integer) }
+  it { is_expected.to define_attribute(:special_seats).as(:integer) }
   it { is_expected.to define_attribute(:storage_spaces).as(:integer) }
+  it { is_expected.to define_attribute(:suitcase_storage_spaces).as(:integer) }
   it { is_expected.to define_attribute(:walker_bays).as(:integer) }
+  it { is_expected.to define_attribute(:walker_storage_spaces).as(:integer) }
   it { is_expected.to define_attribute(:wheelchair_bays).as(:integer) }
+  it { is_expected.to define_attribute(:wheelchair_storage_spaces).as(:integer) }
+  it { is_expected.to define_attribute(:wlan_access_points).as(:integer) }
 end
