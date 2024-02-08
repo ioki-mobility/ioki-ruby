af768fb1a2c5339d8b1813a2cc8481ea54c8d7e6
Tobias Matz <tobias.matz@ioki.com>
2024-02-08 11:27:10 +0100

    Add RematchingAttempt model

---
+++ lib/ioki/model/operator/rematching_attempt.rb
@@ -0,0 +1,76 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class RematchingAttempt < Base
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :auto_cancel_empty_task_list,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :boolean
+
+        attribute :auto_cancel_failed_rematchings,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :boolean
+
+        attribute :finished_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :preserve_negotiated_times,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :boolean
+
+        attribute :reassigner_id,
+                  on:   :read,
+                  type: :string
+
+        attribute :reassigner_type,
+                  on:   :read,
+                  type: :string
+
+        attribute :result,
+                  on:         :read,
+                  type:       :object,
+                  class_name: 'RematchingAttemptResult'
+
+        attribute :ride_ids,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :array
+
+        attribute :started_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :task_list_id,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :string
+
+        attribute :white_listed_task_list_ids,
+                  on:             [:create, :read],
+                  omit_if_nil_on: [:create],
+                  type:           :array
+      end
+    end
+  end
+end
