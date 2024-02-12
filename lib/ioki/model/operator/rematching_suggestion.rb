90ee72ad34483856dea8f67d3bc8a7a5c9bf9391
Tobias Matz <tobias.matz@ioki.com>
2024-02-12 14:24:18 +0100

    Add type to RematchingSuggestion

--- lib/ioki/model/operator/rematching_suggestion.rb
+++ lib/ioki/model/operator/rematching_suggestion.rb
@@ -4,6 +4,10 @@ module Ioki
   module Model
     module Operator
       class RematchingSuggestion < Base
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
         attribute :task_lists,
                   on:         :read,
                   type:       :array,
--- spec/ioki/model/operator/rematching_suggestion_spec.rb
+++ spec/ioki/model/operator/rematching_suggestion_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe Ioki::Model::Operator::RematchingSuggestion do
+  it { is_expected.to define_attribute(:type).as(:string) }
   it { is_expected.to define_attribute(:task_lists).as(:array).with(class_name: 'TaskList') }
 end
