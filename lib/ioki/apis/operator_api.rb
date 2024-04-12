7330067cd575ca744ed25e599025274429a7761c
Tobias Matz <tobias.matz@ioki.com>
2024-04-12 10:04:51 +0200

    Fix linting issues

--- lib/ioki/apis/operator_api.rb
+++ lib/ioki/apis/operator_api.rb
@@ -300,7 +300,7 @@ module Ioki
       ),
       Endpoints::ShowSingular.new(
         :fleet_state,
-        base_path:        [API_BASE_PATH],
+        base_path:   [API_BASE_PATH],
         model_class: Ioki::Model::Operator::Vehicle
       )
     ].freeze
