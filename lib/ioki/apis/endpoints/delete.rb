244f78f882ed6ddea98503ea2e609f15382fa6e5
Eike Send <mail@eike.se>
2024-06-04 12:28:11 +0200

    use Support#blank? method to determine if parsed_response is empty or nil

--- lib/ioki/apis/endpoints/delete.rb
+++ lib/ioki/apis/endpoints/delete.rb
@@ -31,7 +31,7 @@ module Ioki
           params: options[:params]
         )
 
-        return if parsed_response.empty?
+        return if Support.blank?(parsed_response)
 
         model_class
           .new(parsed_response['data'], nil, show_deprecation_warnings: false)
