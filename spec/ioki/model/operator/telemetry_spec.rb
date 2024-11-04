f74531615b2a9b218a1cfeb56edbc051827e9baf
Tobias Matz <tobias.matz@ioki.com>
2024-11-04 22:31:41 +0100

    Add more attributes to Telemetry model

--- spec/ioki/model/operator/telemetry_spec.rb
+++ spec/ioki/model/operator/telemetry_spec.rb
@@ -6,5 +6,4 @@ RSpec.describe Ioki::Model::Operator::Telemetry do
   it { is_expected.to define_attribute(:remaining_distance).as(:float) }
   it { is_expected.to define_attribute(:charging_state).as(:string) }
   it { is_expected.to define_attribute(:estimated_time_until_full_recharge).as(:float) }
-  it { is_expected.to define_attribute(:last_synced_at).as(:date_time) }
 end
