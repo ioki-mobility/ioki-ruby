5ef8650c4b0b6c00b1ca1ec41130188999cb6434
Eike Send <mail@eike.se>
2025-05-06 15:56:23 +0200

    Add all missing schema_path / unvalidated to all models (#286)

--- lib/ioki/model/passenger/payment_method_details.rb
+++ lib/ioki/model/passenger/payment_method_details.rb
@@ -4,6 +4,10 @@ module Ioki
   module Model
     module Passenger
       class PaymentMethodDetails < Base
+        def self.schema_path
+          'passenger_api--payment_method_details_schema'
+        end
+
         attribute :braintree_nonce, on: :create, type: :string, omit_if_nil_on: :create
         attribute :paypal_secure_element, on: :create, type: :string, omit_if_nil_on: :create
         attribute :stripe_payment_method_id, on: :create, type: :string, omit_if_nil_on: :create
