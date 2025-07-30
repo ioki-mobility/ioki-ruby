14a09fb14a8202f68c959f7c4294d20ae4d6b590
Tim Engelhardt <tim.engelhardt@ioki.com>
2025-07-30 16:45:12 +0200

    Remove products from path again

--- lib/ioki/apis/platform_api.rb
+++ lib/ioki/apis/platform_api.rb
@@ -296,7 +296,7 @@ module Ioki
       ),
       Endpoints::ShowSingular.new(
         :fleet_state,
-        base_path:   [API_BASE_PATH, 'products', :id],
+        base_path:   [API_BASE_PATH],
         model_class: Ioki::Model::Platform::FleetState
       )
     ].freeze
