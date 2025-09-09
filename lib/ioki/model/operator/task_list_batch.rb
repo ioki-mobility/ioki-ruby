c7c45dbef6e67b985531eed2c755e198e8fdcde3
Tobias Matz <tobias.matz@ioki.com>
2025-09-09 15:36:13 +0200

    Add TaskListBatch model

---
+++ lib/ioki/model/operator/task_list_batch.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class TaskListBatch < Base
+        def self.schema_path
+          'operator_api--v20210101--task_list_batch_destroy_schema'
+        end
+
+        attribute :task_list_ids,
+                  on:   :delete,
+                  type: :array
+      end
+    end
+  end
+end
