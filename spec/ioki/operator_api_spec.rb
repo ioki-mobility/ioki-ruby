da511af8a0770191cd2d5844f439a9484b1cb71a
Tobias Matz <tobias.matz@ioki.com>
2025-09-09 17:20:57 +0200

    Remove unneeded batch_destroy endpoint and related test

--- spec/ioki/operator_api_spec.rb
+++ spec/ioki/operator_api_spec.rb
@@ -632,19 +632,6 @@ RSpec.describe Ioki::OperatorApi do
     end
   end
 
-  describe '#task_lists_batch_destroy(product_id)' do
-    let(:result_with_data) { nil }
-    it 'calls request on the client with expected params' do
-      expect(operator_client).to receive(:request) do |params|
-        expect(params[:url].to_s).to eq('operator/products/0815/task_lists/batch_destroy')
-        [result_with_data, full_response]
-      end
-
-      expect(operator_client.task_lists_batch_destroy('0815', options))
-        .to be_nil
-    end
-  end
-
   describe 'task_list_batch_destroy(product_id, task_list_batch)' do
     let(:task_list_batch) { Ioki::Model::Operator::TaskListBatch.new(task_list_ids: ['tls_123']) }
     let(:result_with_data) { nil }
