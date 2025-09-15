37fc82c19144b486fffeb5ac3f6b452f5edf20f7
Tobias Matz <tobias.matz@ioki.com>
2025-09-15 10:00:04 +0200

    Add AdminNotification webhook model

---
+++ lib/ioki/model/webhooks/admin_notification.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class AdminNotification < Base
+        attribute :type, type: :string
+        attribute :id, type: :string
+        attribute :created_at, type: :date_time
+        attribute :updated_at, type: :date_time
+        attribute :admin_notification_type, type: :string
+        attribute :title, type: :string
+        attribute :description, type: :string
+        attribute :read_at, type: :date_time
+      end
+    end
+  end
+end
