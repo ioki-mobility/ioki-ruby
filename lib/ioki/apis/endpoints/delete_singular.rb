171c923462c52734c4fd16100d06c68b96948946
Tobias Matz <tobias.matz@ioki.com>
2025-09-22 13:59:33 +0200

    Allow model data to be sent in the body of DELETE requests

--- lib/ioki/apis/endpoints/delete_singular.rb
+++ lib/ioki/apis/endpoints/delete_singular.rb
@@ -6,6 +6,27 @@ module Ioki
       def full_path
         base_path + [resource_path_name]
       end
+
+      def call(client, model, args = [], options = {})
+        outgoing_model_class = @outgoing_model_class || options[:outgoing_model_class] || model_class
+
+        unless model.is_a?(outgoing_model_class)
+          raise(ArgumentError, "#{model} is not an instance of #{outgoing_model_class}")
+        end
+
+        parsed_response, = client.request(
+          url:    client.build_request_url(*Endpoints.url_elements(full_path, *args)),
+          method: :delete,
+          body:   { data: model.serialize(:delete) },
+          params: options[:params]
+        )
+
+        return if parsed_response.nil? || model_class.nil?
+
+        model_class
+          .new(parsed_response['data'], nil, show_deprecation_warnings: false)
+          .tap(&:clear_changes_information)
+      end
     end
   end
 end
