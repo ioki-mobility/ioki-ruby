03149c472265e6f8533a420c34b12f523a44a44d
Tobias Matz <tobias.matz@ioki.com>
2025-09-29 14:47:21 +0200

    Add admin_account and admin_notification models

---
+++ lib/ioki/model/operator/admin_account.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class AdminAccount < Base
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :id,
+                  on:   :read,
+                  type: :string
+
+        attribute :created_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :updated_at,
+                  on:   :read,
+                  type: :date_time
+
+        attribute :email,
+                  on:   :read,
+                  type: :string
+
+        attribute :first_name,
+                  on:   :read,
+                  type: :string
+
+        attribute :last_name,
+                  on:   :read,
+                  type: :string
+
+        attribute :locale,
+                  on:   :read,
+                  type: :string
+
+        attribute :permissions,
+                  on:         :read,
+                  type:       :array,
+                  class_name: 'Ioki::Model::Operator::Permission'
+
+        attribute :admin_notifications,
+                  on:         :read,
+                  type:       :array,
+                  class_name: 'Ioki::Model::Operator::AdminNotification::AdminNotification'
+
+        attribute :consentables,
+                  on:         :read,
+                  type:       :array,
+                  class_name: 'Ioki::Model::Operator::Consentable'
+      end
+    end
+  end
+end
