ea868f1f9aedc47b2a39f7c8158136bd1c403812
Tobias Matz <tobias.matz@ioki.com>
2025-09-30 16:30:22 +0200

    Move UnreadCount webhook model out of AdminNotification namespace

---
+++ lib/ioki/model/webhooks/unread_count.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Webhooks
+      class UnreadCount < Base
+        attribute :type,
+                  on:   :read,
+                  type: :string
+
+        attribute :unread_count,
+                  on:   :read,
+                  type: :integer
+      end
+    end
+  end
+end
