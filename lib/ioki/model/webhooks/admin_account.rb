5483b6c2822365aaba21e0496ed2a53da456050e
Tobias Matz <tobias.matz@ioki.com>
2025-09-30 16:27:43 +0200

    Replace admin_notifications with unread_count attribute

--- lib/ioki/model/webhooks/admin_account.rb
+++ lib/ioki/model/webhooks/admin_account.rb
@@ -4,17 +4,41 @@ module Ioki
   module Model
     module Webhooks
       class AdminAccount < Base
-        attribute :type, type: :string
-        attribute :id, type: :string
-        attribute :created_at, type: :date_time
-        attribute :updated_at, type: :date_time
-        attribute :email, type: :string
-        attribute :first_name, type: :string
-        attribute :last_name, type: :string
-        attribute :locale, type: :string
-        attribute :permissions, type: :array, class_name: 'Ioki::Model::Webhooks::Permission'
-        attribute :admin_notifications, type: :array, class_name: 'Ioki::Model::Webhooks::AdminNotification'
-        attribute :consentables, type: :array, class_name: 'Ioki::Model::Webhooks::Consentable'
+        attribute :type,
+                  type: :string
+
+        attribute :id,
+                  type: :string
+
+        attribute :created_at,
+                  type: :date_time
+
+        attribute :updated_at,
+                  type: :date_time
+
+        attribute :email,
+                  type: :string
+
+        attribute :first_name,
+                  type: :string
+
+        attribute :last_name,
+                  type: :string
+
+        attribute :locale,
+                  type: :string
+
+        attribute :permissions,
+                  type:       :array,
+                  class_name: 'Ioki::Model::Webhooks::Permission'
+
+        attribute :unread_count,
+                  type:       :object,
+                  class_name: 'Ioki::Model::Webhooks::AdminNotification::UnreadCount'
+
+        attribute :consentables,
+                  type:       :array,
+                  class_name: 'Ioki::Model::Webhooks::Consentable'
       end
     end
   end
