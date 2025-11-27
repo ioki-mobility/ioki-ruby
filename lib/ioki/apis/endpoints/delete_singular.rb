7a889a5f97628f8dce48fc812fd32d9f1f79a9f5
Tobias Matz <tobias.matz@ioki.com>
2025-11-27 17:37:04 +0100

    Remove call method from DeleteSingular endpoint

--- lib/ioki/apis/endpoints/delete_singular.rb
+++ lib/ioki/apis/endpoints/delete_singular.rb
@@ -6,21 +6,6 @@ module Ioki
       def full_path
         base_path + [resource_path_name]
       end
-
-      def call(client, args = [], options = {})
-        parsed_response, = client.request(
-          url:    client.build_request_url(*Endpoints.url_elements(full_path, *args)),
-          method: :delete,
-          body:   options[:body],
-          params: options[:params]
-        )
-
-        return if parsed_response.nil? || model_class.nil?
-
-        model_class
-          .new(parsed_response['data'], nil, show_deprecation_warnings: false)
-          .tap(&:clear_changes_information)
-      end
     end
   end
 end
