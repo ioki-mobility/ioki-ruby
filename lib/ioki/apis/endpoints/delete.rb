3b14ede46bffc546cbd55d200f52c6662749d0f3
Tobias Matz <tobias.matz@ioki.com>
2025-12-01 17:33:08 +0100

    Add send_body option to Delete endpoint to control whether to send body

--- lib/ioki/apis/endpoints/delete.rb
+++ lib/ioki/apis/endpoints/delete.rb
@@ -5,12 +5,13 @@ module Ioki
     class Delete
       attr_reader :base_path, :model_class, :resource, :path
 
-      def initialize(resource, base_path:, model_class:, outgoing_model_class: nil, path: nil)
+      def initialize(resource, base_path:, model_class:, **options)
         @base_path = base_path
         @resource = resource.to_s
         @model_class = model_class
-        @outgoing_model_class = outgoing_model_class
-        @path = path
+        @outgoing_model_class = options[:outgoing_model_class]
+        @path = options[:path]
+        @send_body = options.fetch(:send_body, false)
       end
 
       def name
@@ -32,10 +33,12 @@ module Ioki
           raise(ArgumentError, "#{model} is not an instance of #{outgoing_model_class}")
         end
 
+        body = @send_body && model ? { data: model.serialize(:delete) } : nil
+
         parsed_response, = client.request(
           url:    client.build_request_url(*Endpoints.url_elements(full_path, *args)),
           method: :delete,
-          body:   { data: model&.serialize(:delete) },
+          body:,
           params: options[:params]
         )
 
--- spec/ioki/endpoints/delete_spec.rb
+++ spec/ioki/endpoints/delete_spec.rb
@@ -3,18 +3,16 @@
 RSpec.describe Ioki::Endpoints::Delete do
   let(:model_class) { Ioki::Model::Platform::Product }
   let(:client)      { instance_double(Ioki::Client, 'client', build_request_url: 'url') }
-  let(:endpoint)    { described_class.new('product', base_path: ['base_path'], model_class: model_class) }
+  let(:endpoint)    { described_class.new('product', base_path: ['base_path'], model_class: model_class, send_body:) }
   let(:params)      { instance_double(Hash, 'params') }
   let(:response)    { { 'data' => { 'id' => '0815' } } }
 
-  context 'when no model instance is passed' do
-    let(:model)       { nil }
-    let(:serialized_model) { nil }
+  shared_examples 'delete endpoint request and response' do
     it 'calls #request on the client instantiating a class from the result' do
       expect(client).to receive(:request).with(
         url:    'url',
         method: :delete,
-        body:   { data: serialized_model },
+        body:   nil,
         params: params
       ).and_return(response)
 
@@ -24,15 +22,33 @@ RSpec.describe Ioki::Endpoints::Delete do
       expect(result.changes).to be_empty
       expect(result.id).to eq('0815')
     end
+  end
+
+  context 'when no model instance is passed' do
+    let(:model) { nil }
+    let(:serialized_model) { nil }
+
+    context 'and send_body is false' do
+      let(:send_body) { false }
+
+      it_behaves_like 'delete endpoint request and response'
+    end
+
+    context 'and send_body is true' do
+      let(:send_body) { true }
+
+      it_behaves_like 'delete endpoint request and response'
+    end
 
     context 'when the response body is empty' do
       let(:response) { nil }
+      let(:send_body) { false }
 
       it 'returns nil' do
         expect(client).to receive(:request).with(
           url:    'url',
           method: :delete,
-          body:   { data: serialized_model },
+          body:   nil,
           params: params
         ).and_return(response)
 
@@ -46,22 +62,33 @@ RSpec.describe Ioki::Endpoints::Delete do
   context 'when a model instance is passed' do
     let(:model) { Ioki::Model::Platform::Product.new }
     let(:serialized_model) { { name: 'To be deleted' } }
+    let(:send_body) { true }
 
     before { allow(model).to receive(:serialize).with(:delete).and_return(serialized_model) }
 
-    it 'is a shortcut calling #request, sending serialized modeldata and instantiating a new class from the result' do
-      expect(client).to receive(:request).with(
-        url:    'url',
-        method: :delete,
-        body:   { data: serialized_model },
-        params: params
-      ).and_return(response)
+    context 'and send_body is false' do
+      let(:send_body) { false }
 
-      result = endpoint.call(client, model, ['0815'], params: params)
+      it_behaves_like 'delete endpoint request and response'
+    end
 
-      expect(result).to be_a(Ioki::Model::Platform::Product)
-      expect(result.changes).to be_empty
-      expect(result).not_to eq(model)
+    context 'and send_body is true' do
+      let(:send_body) { true }
+
+      it 'is a shortcut calling #request, sending serialized modeldata and instantiating a new class from the result' do
+        expect(client).to receive(:request).with(
+          url:    'url',
+          method: :delete,
+          body:   { data: serialized_model },
+          params: params
+        ).and_return(response)
+
+        result = endpoint.call(client, model, ['0815'], params: params)
+
+        expect(result).to be_a(Ioki::Model::Platform::Product)
+        expect(result.changes).to be_empty
+        expect(result).not_to eq(model)
+      end
     end
   end
 end
