7a0a59ec1c382fe1730ffd1da593b2e9466eb724
Serge Hänni <serge@nyi.ch>
2026-01-15 12:16:15 +0100

    Add matching state to operator API

---
+++ lib/ioki/model/operator/matching_state.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Ioki
+  module Model
+    module Operator
+      class MatchingState < Base
+        attribute :type, on: :read, type: :string
+        attribute :id, on: :read, type: :string
+        attribute :created_at, on: :read, type: :date_time
+        attribute :updated_at, on: :read, type: :date_time
+
+        attribute :logs, on: :read, type: :array, class_name: 'MatchingStateLog'
+        attribute :final_summary, on: :read, type: :string
+        attribute :current_round, on: :read, type: :integer
+        attribute :num_ptas_in_round, on: :read, type: :integer
+        attribute :num_combinations_in_round, on: :read, type: :integer
+        attribute :search_space_time_min_in_round, on: :read, type: :date_time
+        attribute :search_space_time_max_in_round, on: :read, type: :date_time
+        attribute :num_ptas_overall, on: :read, type: :integer
+        attribute :num_combinations_overall, on: :read, type: :integer
+        attribute :search_space_time_min_overall, on: :read, type: :date_time
+        attribute :search_space_time_max_overall, on: :read, type: :date_time
+      end
+    end
+  end
+end
