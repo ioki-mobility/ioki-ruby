1fde9801e73207e9ea9c7b342db320af24ab33d2
Tim Engelhardt <tim.engelhardt@ioki.com>
2026-06-11 18:26:19 +0200

    Add eligibility group memberships

---
+++ spec/ioki/model/operator/eligibility_group_membership_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Ioki::Model::Operator::EligibilityGroupMembership do
+  it { is_expected.to define_attribute(:email_address).as(:string) }
+  it { is_expected.to define_attribute(:user_segment_slug).as(:string) }
+  it { is_expected.to define_attribute(:eligibility_group_slug).as(:string) }
+  it { is_expected.to define_attribute(:id).as(:string) }
+  it { is_expected.to define_attribute(:type).as(:string) }
+  it { is_expected.to define_attribute(:created_at).as(:date_time) }
+  it { is_expected.to define_attribute(:updated_at).as(:date_time) }
+  it { is_expected.to define_attribute(:user_id).as(:string) }
+  it { is_expected.to define_attribute(:state).as(:string) }
+  it { is_expected.to define_attribute(:active_strategies).as(:array) }
+  it { is_expected.to define_attribute(:activated_at).as(:date_time) }
+end
